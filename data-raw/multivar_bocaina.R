70dad56022206b8a0df06c5f1ff7fc2197ed7c62
Gustavo Paterno <paternogbc@gmail.com>
2021-03-10 20:47:19 +0100

    include new data for chap. 8

--- data-raw/multivar_bocaina.R
+++ data-raw/multivar_bocaina.R
@@ -1,7 +1,27 @@
 ## code to prepare `multivar_bocaina` dataset goes here
 library(tidyverse)
 
+# bocaina--------------------------------------------------------
+bocaina <- read.table("data-raw/bocaina.txt", header = TRUE)
+head(bocaina)
+usethis::use_data(bocaina, overwrite = TRUE)
 
-multivar_bocaina <- read.table("data-raw/multivar_bocaina.txt", header = TRUE)
+# altitude--------------------------------------------------------
+altitude <- read.csv("data-raw/altitude.csv", row.names = "X")
+head(altitude)
+usethis::use_data(altitude, overwrite = TRUE)
 
-usethis::use_data(multivar_bocaina, overwrite = TRUE)
+# -------------------------------------------------------------------------
+com_birds <- read.csv("data-raw/com_birds.csv", sep=";", row.names = "X")
+head(com_birds)
+usethis::use_data(com_birds, overwrite = TRUE)
+
+# -------------------------------------------------------------------------
+env_birds <- read.csv("data-raw/env_birds.csv", sep=";", row.names = "X")
+head(env_birds)
+usethis::use_data(env_birds, overwrite = TRUE)
+
+# -------------------------------------------------------------------------
+birds.xy <- read.csv("data-raw/birds.xy.csv", sep=";", row.names = "X")
+head(birds.xy)
+usethis::use_data(birds.xy, overwrite = TRUE)
