56ae03d4fd9901942d62dc629775eba2d3f7bb2f
Gustavo Paterno <paternogbc@gmail.com>
2021-08-18 09:26:45 +0200

    update encoding of some tables : encoding = "latin1"

--- data-raw/multivar_bocaina.R
+++ data-raw/multivar_bocaina.R
@@ -7,7 +7,7 @@ head(bocaina)
 usethis::use_data(bocaina, overwrite = TRUE)
 
 # altitude--------------------------------------------------------
-altitude <- read.csv("data-raw/altitude.csv", row.names = "X")
+altitude <- read.csv("data-raw/altitude.csv", row.names = "X",  encoding = "latin1")
 head(altitude)
 usethis::use_data(altitude, overwrite = TRUE)
 
@@ -17,7 +17,7 @@ head(com_birds)
 usethis::use_data(com_birds, overwrite = TRUE)
 
 # -------------------------------------------------------------------------
-env_birds <- read.csv("data-raw/env_birds.csv", sep=";", row.names = "X")
+env_birds <- read.csv("data-raw/env_birds.csv", sep=";", row.names = "X",  encoding = "latin1")
 head(env_birds)
 usethis::use_data(env_birds, overwrite = TRUE)
 
