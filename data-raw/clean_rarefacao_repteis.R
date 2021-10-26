50f56ec1411d1d83e1be3d550cb8f4501768b67d
Gustavo Paterno <paternogbc@gmail.com>
2021-10-26 10:09:44 +0200

    rarefacao amostras update

--- data-raw/clean_rarefacao_repteis.R
+++ data-raw/clean_rarefacao_repteis.R
@@ -5,3 +5,8 @@ library(tidyverse)
 rarefacao_repteis <- read.csv("data-raw/rarefacao_repteis.csv", header = T, row.names = 1)
 
 usethis::use_data(rarefacao_repteis, overwrite = TRUE)
+
+
+morcegos_rarefacao_amostras <- read.csv("data-raw/morcegos_rarefacao_amostras.csv",  row.names = 1, header = T, sep = ";")
+
+usethis::use_data(morcegos_rarefacao_amostras, overwrite = TRUE)
