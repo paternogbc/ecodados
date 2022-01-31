4060ae173f50e2e4fedab554989bdaec4e41a551
Gustavo Brant Paterno <paternogbc@gmail.com>
2022-01-31 11:46:11 +0100

    add multivar data

--- data-raw/multivar_bocaina.R
+++ data-raw/multivar_bocaina.R
@@ -8,8 +8,8 @@ usethis::use_data(bocaina, overwrite = TRUE)
 
 # bocaina--------------------------------------------------------
 bocaina.env <- read.csv("data-raw/bocaina_env.csv",  sep = ";", row.names = "X")
-head(bocaina_env)
-usethis::use_data(bocaina_env, overwrite = TRUE)
+head(bocaina.env)
+usethis::use_data(bocaina.env, overwrite = TRUE)
 
 # altitude--------------------------------------------------------
 altitude <- read.csv("data-raw/altitude.csv", row.names = "X", encoding = "latin1")
@@ -31,3 +31,17 @@ usethis::use_data(env_birds, overwrite = TRUE)
 birds.xy <- read.csv("data-raw/birds.xy.csv", sep=";", row.names = "X")
 head(birds.xy)
 usethis::use_data(birds.xy, overwrite = TRUE)
+
+# -------------------------------------------------------------------------
+bocaina.xy       <- read.table("data-raw/bocaina_xy.txt", h=T)
+usethis::use_data(bocaina.xy, overwrite = TRUE)
+
+# -------------------------------------------------------------------------
+
+anuros_permanova <- read.csv("data-raw/anuros_permanova.csv", sep=",", row.names = "X")
+usethis::use_data(anuros_permanova, overwrite = TRUE)
+
+
+# -------------------------------------------------------------------------
+macroinv          <- read.csv("data-raw/macroinv.csv", row.names = "X")
+usethis::use_data(macroinv, overwrite = TRUE)
