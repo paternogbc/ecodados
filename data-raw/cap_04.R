7f3c34b6d1d05066d34262a5f381ff52d72844f6
Gustavo Brant Paterno <paternogbc@gmail.com>
2022-05-04 14:08:49 +0200

    add dados: tidy_anfibios_locais

--- data-raw/cap_04.R
+++ data-raw/cap_04.R
@@ -5,3 +5,8 @@ library(tidyverse)
 intror_anfibios_locais <-  read.csv("data-raw/ATLANTIC_AMPHIBIANS_sites.csv", encoding = "latin1")
 
 usethis::use_data(intror_anfibios_locais, overwrite = TRUE)
+
+# tidy_anfibios
+tidy_anfibios_locais <- readr::read_csv(here::here("data-raw", "ATLANTIC_AMPHIBIANS_sites.csv"),
+  locale = readr::locale(encoding = "latin1"))
+usethis::use_data(tidy_anfibios_locais, overwrite = TRUE)
